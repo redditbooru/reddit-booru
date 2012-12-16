596347541d05eb5037a9761f2cb176cd5f49849b
Matt Hackmann <matt.hackmann@gmail.com>
2012-12-16 14:06:41 -0600

    Additional settings added for reddit/minus API usage

--- config.php
+++ config.php
@@ -11,5 +11,14 @@ define('AWS_KEY', 'AWS_KEY');
 define('AWS_SECRET', 'AWS_SECRET');
 define('AWS_ENABLED', true);
 
+define('REDDIT_TOKEN', 'REDDIT_TOKEN');
+define('REDDIT_SECRET', 'REDDIT_SECRET');
+define('REDDIT_USER', 'REDDIT_USER');
+
+define('MINUS_USERNAME', 'MINUS_USERNAME');
+define('MINUS_PASSWORD', 'MINUS_PASSWORD');
+define('MINUS_API_KEY', 'KEY');
+define('MINUS_API_SECRET', 'SECRET');
+
 // View directory
 define('VIEW_PATH', './view/');
