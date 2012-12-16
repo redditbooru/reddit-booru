0dbac494c67918aa10a6e02baabaed7e78899b87
Matt Hackmann <matt.hackmann@gmail.com>
2012-12-16 14:02:58 -0600

    Fixed namespacing issues to Db calls, GET requests use logged in user where applicable

--- api/reddit.php
+++ api/reddit.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace Api {
-
+	
+	use Lib;
+	use stdClass;
+	
 	/**
 	 * Reddit object types
 	 */
@@ -137,10 +140,10 @@ namespace Api {
 		 * @param string $page The page to get. Ex: 'r/anime' returns the data object for the anime subreddit
 		 * @return object Returns the data object retrieved
 		 */
-		public function GetPageListing($page, $count = 0, $afterId = null) {
+		public function GetPageListing($page, $count = 0, $afterId = null, $limit = 25) {
 			$retVal = false;
 			$count *= 25;
-			$file = self::curl_get_contents('http://www.reddit.com/' . $page . '.json?count=' . $count . '&after=' . $afterId);
+			$file = self::curl_get_contents('http://www.reddit.com/' . $page . '.json?count=' . $count . '&after=' . $afterId . '&limit=' . $limit, $this->_cookie);
 			echo 'http://www.reddit.com/' . $page . '.json?count=' . $count . '&afterId=' . $afterId, PHP_EOL;
 			if (strlen($file) > 0) {
 				$obj = json_decode($file);
@@ -182,8 +185,8 @@ namespace Api {
 		private function _loadSessionById($id) {
 			$retVal = false;
 			
-			$result = Db::Query('SELECT bot_name, bot_password, bot_hash, bot_cookie, bot_data, bot_updated, bot_callback FROM bot_users WHERE bot_id=:id LIMIT 1', array(':id'=>$id));
-			while ($row = Db::Fetch($result)) {
+			$result = Lib\Db::Query('SELECT bot_name, bot_password, bot_hash, bot_cookie, bot_data, bot_updated, bot_callback FROM bot_users WHERE bot_id=:id LIMIT 1', array(':id'=>$id));
+			while ($row = Lib\Db::Fetch($result)) {
 				$this->_userName = $row->bot_name;
 				$this->_password = $row->bot_password;
 				$this->_hash = $row->bot_hash;
@@ -203,8 +206,8 @@ namespace Api {
 		private function _loadSessionByUserName($userName) {
 			$retVal = false;
 			
-			$result = Db::Query('SELECT bot_name, bot_password, bot_hash, bot_cookie, bot_data, bot_updated, bot_callback FROM bot_users WHERE bot_name=:name LIMIT 1', array(':name'=>$userName));
-			while ($row = Db::Fetch($result)) {
+			$result = Lib\Db::Query('SELECT bot_name, bot_password, bot_hash, bot_cookie, bot_data, bot_updated, bot_callback FROM bot_users WHERE bot_name=:name LIMIT 1', array(':name'=>$userName));
+			while ($row = Lib\Db::Fetch($result)) {
 				$this->_userName = $row->bot_name;
 				$this->_password = $row->bot_password;
 				$this->_userObj = new stdClass();
@@ -224,12 +227,12 @@ namespace Api {
 		 */
 		private function _persist() {
 			$retVal = false;
-			$result = Db::Query('SELECT bot_id FROM bot_users WHERE bot_name=:user', array(':user'=>$this->_userName));
+			$result = Lib\Db::Query('SELECT bot_id FROM bot_users WHERE bot_name=:user', array(':user'=>$this->_userName));
 			if ($result->count > 0) {
 				$params = array(':hash'=>$this->_hash, ':cookie'=>$this->_cookie, ':name'=>$this->_userName, ':data'=>$this->data);
-				$retVal = Db::Query('UPDATE bot_users SET bot_updated=UNIX_TIMESTAMP(NOW()), bot_hash=:hash, bot_cookie=:cookie, bot_data=:data WHERE bot_name=:name', $params) > 0;
+				$retVal = Lib\Db::Query('UPDATE bot_users SET bot_updated=UNIX_TIMESTAMP(NOW()), bot_hash=:hash, bot_cookie=:cookie, bot_data=:data WHERE bot_name=:name', $params) > 0;
 			} else {
-				$retVal = Db::Query('INSERT INTO bot_users (bot_created, bot_name, bot_password, bot_cookie, bot_hash, bot_enabled) VALUES (UNIX_TIMESTAMP(NOW()), :user, :password, :cookie, :hash, 0)', array(':password'=>$this->_password, ':hash'=>$this->_hash, ':cookie'=>$this->_cookie, ':user'=>$this->_userName)) > 0;
+				$retVal = Lib\Db::Query('INSERT INTO bot_users (bot_created, bot_name, bot_password, bot_cookie, bot_hash, bot_enabled) VALUES (UNIX_TIMESTAMP(NOW()), :user, :password, :cookie, :hash, 0)', array(':password'=>$this->_password, ':hash'=>$this->_hash, ':cookie'=>$this->_cookie, ':user'=>$this->_userName)) > 0;
 			}
 		}
 		
@@ -267,11 +270,14 @@ namespace Api {
 		 * @param string $url Url to retrieve
 		 * @return string Data received
 		 */
-		private static function curl_get_contents($url) {
+		private static function curl_get_contents($url, $cookie = null) {
 			$c = curl_init($url);
 			curl_setopt($c, CURLOPT_USERAGENT, 'moe downloader by /u/dxprog');
 			curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
 			curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+			if (null !== $cookie) {
+				curl_setopt($c, CURLOPT_COOKIE, 'reddit_session=' . $cookie);
+			}
 			curl_setopt($c, CURLOPT_TIMEOUT, 5);
 			return curl_exec($c);
 		}
