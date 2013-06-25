b33abdea197203f2637e7075c8a010f3a9e84558
Matt Hackmann <matt.hackmann@gmail.com>
2013-06-25 02:50:04 -0500

    Generic mapped object constructor, generic getById function, copyFromDbRow makes the primary key type int

--- lib/dal.php
+++ lib/dal.php
@@ -2,8 +2,23 @@
 
 namespace Lib {
 	
+    use Exception;
+
 	class Dal {
 		
+        /**
+         * Constructor
+         */
+        public function __construct($obj = null) {
+        
+            if (is_numeric($obj)) {
+                $this->getById($obj);
+            } else if (is_object($obj)) {
+                $this->copyFromDbRow($obj);
+            }
+        
+        }
+        
 		/**
 		 * Syncs the current object to the database
 		 */
@@ -19,12 +34,12 @@ namespace Lib {
 				$primaryKey = property_exists($this, '_dbPrimaryKey') ? $this->_dbPrimaryKey : false;
 				$primaryKeyValue = 0;
 				if ($primaryKey) {
-					$primaryKeyValue = $this->$primaryKey;
+					$primaryKeyValue = (int) $this->$primaryKey;
 				}
 				
 				// If the primary key value is non-zero, do an UPDATE
 				$method = $primaryKeyValue !== 0 ? 'UPDATE' : 'INSERT';
-				$parameters = array();
+				$parameters = [];
 				
 				foreach ($this->_dbMap as $property => $column) {
 					// Primary only gets dropped in for UPDATEs
@@ -75,10 +90,41 @@ namespace Lib {
 				foreach($this->_dbMap as $property => $column) {
 					if (property_exists($obj, $column) && property_exists($this, $property)) {
 						$this->$property = $obj->$column;
+						if ($column === $this->_dbPrimaryKey) {
+							$this->$property = (int) $this->$property;
+						}
 					}
 				}
 			}
 		}
+        
+        /**
+         * Gets a record from the database by the primary key
+         */
+        public function getById($id) {
+
+            $retVal = null;
+            if (property_exists($this, '_dbTable') && property_exists($this, '_dbMap') && property_exists($this, '_dbPrimaryKey') && is_numeric($id)) {
+
+                $cacheKey = $this->_dbTable . '_getById_' . $id;
+                $retVal = Cache::Get($cacheKey);
+
+                if (!$retVal) {
+                    $query  = 'SELECT ' . implode(',', $this->_dbMap) . ' FROM `' . $this->_dbTable . '` ';
+                    $query .= 'WHERE ' . $this->_dbMap[$this->_dbPrimaryKey] . ' = :id LIMIT 1';
+                    
+                    $result = Db::Query($query, [ ':id' => $id ]);
+                    if (null != $result && $result->count === 1) {
+                        $retVal = $this->copyFromDbRow(Db::Fetch($result));
+                    }
+                    Cache::Set($cacheKey, $retVal);
+                }
+
+            } else {
+                throw new Exception('Class must have "_dbTable", "_dbMap", and "_dbPrimaryKey" properties to use method "getById"');
+            }
+            return $retVal;
+        }
 	
 	}
 
