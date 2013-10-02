c03576c69d310415d0af8d38664005ff9402c84e
Matt Hackmann <matt.hackmann@gmail.com>
2013-10-01 23:09:27 -0500

    Change to support new URL naming scheme

--- thumb.php
+++ thumb.php
@@ -11,14 +11,20 @@ $overflow = isset($_GET['overflow']) ? $_GET['overflow'] : 'crop';
 
 if ($file && ($outWidth || $outHeight)) {
 
-	$outFile = md5($file . '_' . $outWidth . '_' . $outHeight) . '.jpg';
+	// The scheme for URL naming is set so that all the parameters can be inferred from the file name. They are
+	// - URL of file, base64 encoded with trailing == removed
+	// - height and width of thumbnail
+	$encodedUrl = base64_encode($file);
+	$encodedUrl = substr($encodedUrl, 0, strlen($encodedUrl) - 2);
+
+	$outFile = $encodedUrl . '_' . $outWidth . '_' . $outHeight . '.jpg';
 
 	// Check to see if there's a cached thumbnail already
 	if (file_exists ('cache/' . $outFile) && filemtime('cache/' . $outFile) > strtotime('-7 days') && filesize('cache/' . $outFile) > 1024) {
 		header ('Location: cache/' . $outFile);
 		exit;
 	}
-	
+
     $image = new Imagick($file);
     if ($image) {
 
@@ -28,7 +34,7 @@ if ($file && ($outWidth || $outHeight)) {
                 break;
             }
         }
-    
+
         $image->cropThumbnailImage($outWidth, $outHeight);
         $image->setFormat('JPEG');
         $handle = fopen('cache/' . $outFile, 'wb');
@@ -36,5 +42,5 @@ if ($file && ($outWidth || $outHeight)) {
         fclose($handle);
         header('Location: cache/' . $outFile);
     }
-	
+
 }
