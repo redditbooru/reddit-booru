a81de7700007a33a147a60a89444d14dd364b2df
Matt Hackmann <matt.hackmann@gmail.com>
2014-03-20 21:53:58 -0700

    image processing routine implemented

--- cron/cron.php
+++ cron/cron.php
@@ -31,7 +31,61 @@ function parseArgs($argv, $argc) {
 }
 
 /**
- *
+ * Downloads, processes, and saves image information and returns the row ID
+ */
+function processImage($url) {
+
+    $retVal = null;
+
+    $logHead = '[ ' . $url . ' ] ';
+
+    // Check to see if this image is already in the database
+    $result = Api\Image::query([ 'url' => $url ]);
+    if (null !== $result && $result->count) {
+        _log($logHead . 'Found in database');
+        $image = new Api\Image(Lib\Db::Fetch($result));
+        $retVal = (int) $image->id;
+    } else {
+
+        // Attempt a fetch of the image
+        _log($logHead . 'Downloading...');
+        $imageData = Lib\ImageLoader::fetchImage($url);
+        if (null !== $imageData) {
+
+            // Generate the histogram
+            _log($logHead . 'Processing...');
+            $image = Api\Image::createFromBuffer($imageData->data);
+            if (null !== $image) {
+                $image->url = $url;
+                $image->type = $imageData->type;
+
+                // Save to the database
+                if ($image->sync()) {
+
+                    // Save all the various copies of the image
+                    $retVal = $image->id;
+                    _log($logHead . 'DONE');
+
+                } else {
+                    _log($logHead . 'Unable to sync image to database');
+                }
+
+            } else {
+                _log($logHead . 'Unable to process image');
+            }
+
+        } else {
+            _log($logHead . 'Unable to download image');
+        }
+
+    }
+
+    return $retVal;
+
+}
+
+/**
+ * Processes all the images in a post
  */
 function processPost($post, $source) {
 
@@ -39,6 +93,17 @@ function processPost($post, $source) {
     $images = Lib\ImageLoader::getImagesFromUrl($post->url);
     if (is_array($images) && count($images) > 0) {
         _log('[ ' . $post->id . ' ] resolved to ' . count($images) . ' images');
+
+        // Process each image
+        $ids = [];
+        foreach ($images as $image) {
+            _log('[ ' . $post->id . ' ] Processing image ' . $image);
+            $id = processImage($image);
+            if ($id) {
+                $ids[] = $id;
+            }
+        }
+
     }
 
 }
