61d1beddeee2a5e871c9175dfba7c28e1477d71b
Matt Hackmann <matt.hackmann@gmail.com>
2014-03-22 00:47:46 -0700

    post saving/updating added

--- cron/cron.php
+++ cron/cron.php
@@ -13,6 +13,11 @@ function _log($message) {
     echo date('[Y-m-d h:i:s]'), ' ' . $message, PHP_EOL;
 }
 
+function _logDbError($header) {
+    print_r(Lib\Db::$lastError); exit;
+    _log($header . Lib\Db::$lastError);
+}
+
 /**
  * Parses the command line arguments
  */
@@ -43,8 +48,7 @@ function processImage($url) {
     $result = Api\Image::query([ 'url' => $url ]);
     if (null !== $result && $result->count) {
         _log($logHead . 'Found in database');
-        $image = new Api\Image(Lib\Db::Fetch($result));
-        $retVal = (int) $image->id;
+        $retVal = new Api\Image(Lib\Db::Fetch($result));
     } else {
 
         // Attempt a fetch of the image
@@ -63,7 +67,7 @@ function processImage($url) {
                 if ($image->sync()) {
 
                     // Save all the various copies of the image
-                    $retVal = $image->id;
+                    $retVal = $image;
                     _log($logHead . 'DONE');
 
                 } else {
@@ -89,19 +93,80 @@ function processImage($url) {
  */
 function processPost($post, $source) {
 
-    // Resolve the post link to images
-    $images = Lib\ImageLoader::getImagesFromUrl($post->url);
-    if (is_array($images) && count($images) > 0) {
-        _log('[ ' . $post->id . ' ] resolved to ' . count($images) . ' images');
+    $post = Api\Post::createFromRedditObject($post);
+    $post->sourceId = $source->id;
+    $logHead = '[ ' . $post->externalId . ' ] ';
 
-        // Process each image
-        $ids = [];
-        foreach ($images as $image) {
-            _log('[ ' . $post->id . ' ] Processing image ' . $image);
-            $id = processImage($image);
-            if ($id) {
-                $ids[] = $id;
+    // Check to see if the post has already been processed
+    $result = Api\PostData::query([ 'externalId' => $post->externalId ]);
+    if ($result && $result->count > 0) {
+        _log($logHead . 'Updating...');
+        $row = Lib\Db::Fetch($result);
+        $dataRow = new Api\PostData($row);
+        $dataRow->updateAll($post);
+    } else {
+
+        // Resolve the post link to images
+        $images = Lib\ImageLoader::getImagesFromUrl($post->link);
+        if (is_array($images) && count($images) > 0) {
+            _log($logHead . 'resolved to ' . count($images) . ' images');
+
+            // Process each image
+            $imgObjs = [];
+            foreach ($images as $image) {
+                _log($logHead . 'Processing image ' . $image);
+                $imgObj = processImage($image);
+                if ($imgObj) {
+                    $imgObjs[] = $imgObj;
+                }
             }
+
+            // Create the post object
+            _log($logHead . 'Syncing post to database...');
+            $syncOkay = false;
+            try {
+                $syncOkay = $post->sync();
+            } catch (Exception $e) {
+                // Continue on duplicate entry, it probably means that something else failed down the line
+                // Fetch the post record and try we'll again
+                if ($e->errorInfo[1] === 1062) {
+                    $result = Api\Post::query([ 'externalId' => $post->externalId ]);
+                    if ($result && $result->count > 0) {
+                        $post = new Api\Post(Lib\Db::Fetch($result));
+                        $syncOkay = true;
+                    }
+                }
+            }
+
+            if ($syncOkay && count($imgObjs) > 0) {
+
+                // Assign all the images to the post
+                _log($logHead . 'Assigning images to post...');
+                if (Api\PostImages::assignImagesToPost($imgObjs, $post)) {
+
+                    // Save off denormalized versions of the data
+                    _log($logHead . 'Denormalizing data...');
+                    if (Api\PostData::denormalizeForPost($post->id)) {
+                        _log($logHead . 'DONE');
+                    } else {
+                        _log($logHead . 'Error saving denormalized data');
+                        _logDbError($logHead);
+                    }
+
+                } else {
+                    _log($logHead . 'Error assigning images');
+                    _logDbError($logHead);
+                }
+
+            } else {
+                if (!$syncOkay) {
+                    _log($logHead . 'Error syncing post to database');
+                    _logDbError($logHead);
+                } else {
+                    _log($logHead . 'No valid images for post');
+                }
+            }
+
         }
 
     }
