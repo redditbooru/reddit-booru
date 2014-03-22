2b50ff8880ebe68147ed65fd4e0cc47711b5648d
Matt Hackmann <matt.hackmann@gmail.com>
2014-03-21 23:58:20 -0700

    mimic db.php return values for INSERT/UPDATE

--- test/test_db.php
+++ test/test_db.php
@@ -15,10 +15,23 @@ namespace Lib {
         }
 
         public static function Query($query, $params = null) {
+            
             $retVal = new stdClass;
             $retVal->query = $query;
             $retVal->params = $params;
             self::$lastResult = $retVal;
+
+            // Action specific returns
+            switch (strtolower(current(explode(' ', $sql)))) {
+                case 'insert':
+                    $retVal = 1;
+                    break;
+                case 'update':
+                case 'delete':
+                    $retVal = 1;
+                    break;
+            }
+
             return $retVal;
         }
 
