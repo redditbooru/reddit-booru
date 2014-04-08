9d97936437849ee88e82c2f5b657ff18ecc50cb3
Matt Hackmann <matt.hackmann@gmail.com>
2014-04-07 23:59:42 -0700

    basic functionality stubbed

--- static/js/dev/view/SidebarView.js
+++ static/js/dev/view/SidebarView.js
@@ -1,9 +1,19 @@
 (function (undefined) {
-    
+
     RB.SidebarView = Backbone.View.extend({
 
+        $sidebar: $('#supporting'),
+
         initialize: function() {
-            
+
+        },
+
+        populate: function(html, owner) {
+            this.$sidebar.html(html).addClass('hasContent');
+
+            // TODO - this should probably be event controlled
+            RB.App.views.images.calculateWindowColumns();
+            RB.App.views.images.render();
         }
 
     });
