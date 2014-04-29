08f1a54f137a49a3f1e66359fb33bd2b96c4d778
Matt Hackmann <matt.hackmann@gmail.com>
2014-04-28 21:28:07 -0700

    upload form saving

--- static/js/dev/view/UploadView.js
+++ static/js/dev/view/UploadView.js
@@ -2,6 +2,9 @@
 
     'use strict';
 
+    var SAVE_KEY = 'RB_Uploads',
+        THUMB_LOCATION = 'http://beta.redditbooru.com/cache/'
+
     RB.UploadView = Backbone.View.extend({
 
         $upload: $('#upload'),
@@ -13,6 +16,9 @@
                 .on('change', '#imageUrl', _.bind(this.handleUrlChange, this))
                 .on('click', '.close', _.bind(this._hideDialog, this))
                 .on('click', '#imageFileButton', _.bind(this.handleUploadClick, this));
+
+            this._loadForm();
+
         },
 
         handleClick: function(evt) {
@@ -21,28 +27,32 @@
         },
 
         handleUploadClick: function(evt) {
-            new RB.Uploader(function(data) {
-                console.log('whoa');
-            });
+            var self = this;
+            new RB.Uploader(function(uploadId) {
+                self.$upload.find('ul').append(RB.Templates.uploading({ id: uploadId }));
+                console.log(self);
+            }, _.bind(this._uploadComplete, this));
         },
 
         handleUrlChange: function(evt) {
-            var url = evt.target.value,
-                $upload = this.$upload;
+            var url = evt.target.value;
 
-            $upload.find('ul').append(RB.Templates.uploading({ id: url }));
+            this.$upload.find('ul').append(RB.Templates.uploading({ id: url }));
             $.ajax({
                 url: '/upload/?action=upload&imageUrl=' + escape(url),
                 dataType: 'json',
-                success: function(data) {
-
-                    $upload.find('[data-id="' + url + '"]').replaceWith(RB.Templates.uploadImageInfo({ id: url, thumb: data.thumb }));
-
-                }
+                success: _.bind(this._uploadComplete, this)
             });
 
             setTimeout(_.bind(this._checkProgress, this), 250);
 
+        },
+
+        _uploadComplete: function(data) {
+            if (!data.error) {
+                this.$upload.find('[data-id="' + data.uploadId + '"]').replaceWith(RB.Templates.uploadImageInfo({ id: data.uploadId, thumb: THUMB_LOCATION + data.thumb }));
+                this._saveForm();
+            }
         },
 
         _showDialog: function() {
@@ -72,6 +82,40 @@
                 }
 
                 setTimeout(_.bind(this._checkProgress, this), 1000);
+            }
+
+        },
+
+        /**
+         * Saves the current items to local storage for later
+         */
+        _saveForm: function() {
+            var data = [];
+
+            this.$upload.find('li').each(function() {
+                var $this = $(this),
+                    item = {
+                        id: $this.data('id'),
+                        thumb: $this.find('img').attr('src'),
+                        caption: $this.find('[name="caption[]"]').val(),
+                        source: $this.find('[name="source[]"]').val()
+                    };
+                data.push(item);
+            });
+
+            window.localStorage[SAVE_KEY] = JSON.stringify(data);
+
+        },
+
+        _loadForm: function() {
+            var data = window.localStorage[SAVE_KEY],
+                $list = this.$upload.find('ul');
+
+            if (data) {
+                data = JSON.parse(data);
+                _.each(data, function(item) {
+                    $list.append(RB.Templates.uploadImageInfo(item));
+                });
             }
 
         }
