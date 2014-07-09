ecd95c0523db1b782ddb78292b15d3a6e8f60fff
Matt Hackmann <matt.hackmann@gmail.com>
2014-07-08 22:55:17 -0700

    added template helper

--- lib/testbucket.php
+++ lib/testbucket.php
@@ -9,7 +9,7 @@ namespace Lib {
 
         private static $_tests;
         private static $_initialized = false;
-        private static $_seed;
+        private static $_seed = false;
 
         /**
          * Loads the test buckets and sets the bucket seed
@@ -21,9 +21,37 @@ namespace Lib {
                     return json_decode(@file_get_contents('buckets.json'));
                 }, TEST_BUCKET_CACHE_KEY);
                 self::$_initialized = true;
+
+                // Add template helpers
+                Display::addHelper('inTestBucket', function($template, $context, $args, $source) {
+                    $args = explode(' ', $args);
+                    $argc = count($args);
+
+                    if ($argc === 2) {
+                        for ($i = 0; $i < $argc; $i++) {
+
+                            // Check for literal value vs template variable
+                            if (strpos($args[$i], '"') === 0) {
+                                $args[$i] = str_replace('"', '', $args[$i]);
+                            } else {
+                                $args[$i] = $context->get($args[$i]);
+                            }
+
+                        }
+
+                        if (self::get($args[0]) == $args[1]) {
+                            return $template->render($context);
+                        }
+
+                    }
+
+                });
+
             }
 
-            self::$_seed = $seed ?: (int) str_replace('.', '', $_SERVER['REMOTE_ADDR']);
+            if ($seed || !self::$_seed) {
+                self::$_seed = $seed ?: (int) str_replace('.', '', $_SERVER['REMOTE_ADDR']);
+            }
 
         }
 
@@ -33,6 +61,7 @@ namespace Lib {
         public static function get($key, $seed = null) {
 
             $seed = $seed ?: self::$_seed;
+            $cacheKey = 'test_' . $key . '_' . $seed;
 
             self::initialize();
             return Cache::fetch(function() use ($key, $seed) {
@@ -66,7 +95,7 @@ namespace Lib {
                 }
 
                 return $retVal;
-            }, 'test_' . $key . '_' . $seed);
+            }, $cacheKey);
         }
 
     }
