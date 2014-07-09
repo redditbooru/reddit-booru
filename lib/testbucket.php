d42e08431853479658ad326245c86fabd4b8cfb3
Matt Hackmann <matt.hackmann@gmail.com>
2014-07-08 22:32:07 -0700

    implementation

--- lib/testbucket.php
+++ lib/testbucket.php
@@ -2,31 +2,71 @@
 
 namespace Lib {
 
-    define('TEST_BUCKET_CACHE_KEY', 'bucket_tests')
+    define('TEST_BUCKET_CACHE_KEY', 'bucket_tests');
+    define('DEFAULT_TEST_VALUE', 'control');
 
     class TestBucket {
 
-        private $_tests;
-        private $_initialized;
-        private $_seed;
+        private static $_tests;
+        private static $_initialized = false;
+        private static $_seed;
 
+        /**
+         * Loads the test buckets and sets the bucket seed
+         */
         public static function initialize($seed = null) {
 
             if (!self::$_initialized) {
-                self::$_tests = Cache::fetch(TEST_BUCKET_CACHE_KEY, function() {
+                self::$_tests = Cache::fetch(function() {
                     return json_decode(@file_get_contents('buckets.json'));
-                }, CACHE_MEDIUM);
+                }, TEST_BUCKET_CACHE_KEY);
                 self::$_initialized = true;
-                self::$_seed = $seed ?: (int) str_replace($_SERVER['REMOTE_ADDR'], '.');
             }
+
+            self::$_seed = $seed ?: (int) str_replace('.', '', $_SERVER['REMOTE_ADDR']);
 
         }
 
-        public static function get($key, $seed = self::$_seed) {
+        /**
+         * Gets a test bucket value for the given seed
+         */
+        public static function get($key, $seed = null) {
+
+            $seed = $seed ?: self::$_seed;
+
             self::initialize();
-            if (isset(self::$_tests->$key)) {
-                srand($seed);
-            }
+            return Cache::fetch(function() use ($key, $seed) {
+                $retVal = DEFAULT_TEST_VALUE;
+                $found = false;
+
+                if (isset(self::$_tests->$key)) {
+                    $test = self::$_tests->$key;
+                    if (isset($test->whiteLists)) {
+                        foreach ($test->whiteLists as $whiteList) {
+                            if (in_array($seed, $whiteList->ids)) {
+                                $retVal = $whiteList->value;
+                                $found = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!$found) {
+                        srand($seed);
+                        $rand = rand() % 100;
+                        $percentTotal = 0;
+                        foreach ($test->ramps as $ramp) {
+                            $percentTotal += $ramp->percent;
+                            if ($rand <= $percentTotal) {
+                                $retVal = $ramp->value;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return $retVal;
+            }, 'test_' . $key . '_' . $seed);
         }
 
     }
