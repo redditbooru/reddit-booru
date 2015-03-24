54fb952ff8d775d01befe876d3a11d603f00dfc1
chiefnoah <chiefnoah@users.noreply.github.com>
2015-03-23 21:46:32 -0500

    Changed content type to application/json

--- controller/images.php
+++ controller/images.php
@@ -49,7 +49,7 @@ namespace Controller {
                 header('Access-Control-Allow-Origin: *');
             }
 
-            header('Content-Type: text/javascript; charset=utf-8');
+            header('Content-Type: application/json; charset=utf-8');
             echo json_encode($images);
             exit;
         }
