aaedc5f00e1390506ac91941ab3c5ab811fd5bb2
Matt Hackmann <matt@dxprog.com>
2015-10-21 20:33:22 -0700

    beginning the move to ES6

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,82 +1,56 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: [
-                    'static/js/dev/lib/jquery.min.js',
-                    'static/js/dev/lib/underscore.min.js',
-                    'static/js/dev/lib/backbone.min.js',
-                    'static/js/dev/lib/handlebars.runtime.js',
-                    'static/js/dev/model/*.js',
-                    'static/js/dev/controls/*.js',
-                    'static/js/templates.js',
-                    'static/js/dev/view/*.js',
-                    'static/js/dev/Uploader.js',
-                    'static/js/dev/Routes.js',
-                    'static/js/dev/App.js'
-                ],
-                dest: 'static/js/<%= pkg.name %>.js'
-            }
-        },
         uglify: {
             options: {
                 banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             dist: {
                 files: {
-                    'static/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                    './static/js/<%= pkg.name %>.min.js': [ './static/js/<%= pkg.name %>.js' ]
                 }
             }
         },
         sass: {
             dist: {
                 files: {
-                    'static/scss/styles.css': 'static/scss/styles.scss',
-                    'static/scss/mobile.css': 'static/scss/mobile.scss'
+                    './static/scss/styles.css': './static/scss/styles.scss',
+                    './static/scss/mobile.css': './static/scss/mobile.scss'
                 }
             }
         },
-        handlebars: {
-            compile: {
-                options: {
-                    namespace: 'RB.Templates',
-                    wrapped:true,
-                    processName: function(filename) {
-                        filename = filename.split('/');
-                        filename = filename[filename.length - 1];
-                        return filename.split('.')[0];
-                    }
-                },
-                files: {
-                    'static/js/templates.js': 'views/*.handlebars'
-                }
+        browserify: {
+            options: {
+                transform: [
+                    [ 'babelify', { 'stage': 0 }],
+                    [ 'browserify-handlebars' ]
+                ],
+                require: [
+                    './node_modules/underscore/underscore.js:underscore',
+                    './node_modules/jquery/dist/jquery.js:jquery',
+                    './node_modules/backbone/backbone.js:backbone'
+                ]
+            },
+            dist: {
+                src: [ './static/js/dev/App.js', './views/*.handlebars' ],
+                dest: './static/js/<%= pkg.name %>.js'
             }
         },
         watch: {
             files: [
-                'static/js/dev/lib/*.js',
-                'static/js/dev/model/*.js',
-                'static/js/dev/view/*.js',
-                'static/js/dev/controls/*.js',
-                'static/js/dev/*.js',
-                'views/*.handlebars',
-                'views/partials/*.handlebars',
-                'static/scss/*.scss'
+                './static/js/dev/**/*.js',
+                './views/**/*.handlebars',
+                './static/scss/*.scss'
             ],
-            tasks: ['handlebars', 'concat', 'sass']
+            tasks: ['browserify', 'sass']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-sass');
 
-    grunt.registerTask('default', ['handlebars', 'sass', 'concat', 'uglify']);
+    grunt.registerTask('default', ['browserify', 'sass', 'uglify']);
 
 };
