15ae5b94010fe371640e7017870d796dc690f533
Matt Hackmann <matt@dxprog.com>
2016-08-26 18:26:55 -0700

    added lots of comments to sample configs

--- app-config.sample.php
+++ app-config.sample.php
@@ -1,28 +1,42 @@
 <?php
 
+// This is where the "backend" of RedditBooru lives. A single backend can serve multiple frontends.
 define('CORE_LOCATION', '/var/www/reddit-booru');
 
+// Default page title
 define('DEFAULT_TITLE', '');
-define('DEFAULT_CONTROLLER', 'landing');
 
+// Reddit OAuth information. Register your application at https://www.reddit.com/prefs/apps
 define('REDDIT_TOKEN', 'REDDIT_TOKEN');
 define('REDDIT_SECRET', 'REDDIT_SECRET');
-define('REDDIT_OAUTH_HANDLER', 'http://OAUTH_HANDLE');
-define('REDDIT_USER', 'USER');
+define('REDDIT_OAUTH_HANDLER', 'http://OAUTH_HANDLER');
 
-define('TUMBLR_CONSUMER_KEY', 'TUMBLR_CONSUMER_KEY');
-
+// Cookie domain for sessions. If you're using subdomains, it's recommended to set for all sub domains
 define('SESSION_DOMAIN', '.redditbooru.com');
 
-define('CDN_BASE_URL', 'http://CDN_URL');
-define('LOCAL_IMAGE_PATH', '/LOCAL/IMAGES');
+// Full URL to where images are served from (with trailing slash)
+// Ex: http://cdn.awwni.me/
+define('CDN_BASE_URL', 'http://CDN_URL/');
+
+// Full path to where images are stored locally (with trailing slash)
+define('LOCAL_IMAGE_PATH', '/LOCAL/IMAGES/');
+
+// Relative path to where thumbnail files are served from. Can be on the same domain or separate.
 define('THUMBNAIL_PATH', '/cache/');
+
+// Full path to where thumbnails will be written. This folder should be served
+// where THUMBNAIL_PATH points.
 define('THUMBNAIL_STORAGE', '/FULL/PATH/TO/THUMBNAILS');
 
+// Used to namespace this frontend in caching operations
 define('CACHE_PREFIX', 'RedditBooru');
 
+// Use minified JavaScript. Recommended to be enabled for production, disabled for development
 define('USE_MIN_JS', true);
+
+// Cache buster for JavaScript payload. Just increment on change. TODO - make this work off of
+// md5 hash of JS file instead
 define('JS_VERSION', 1);
 
-// Set the time zone
+// The timezone that all timestamps will be generated from
 date_default_timezone_set('America/Chicago');
--- config.sample.php
+++ config.sample.php
@@ -6,9 +6,12 @@ define('DB_USER', 'USER');
 define('DB_PASS', 'PASSWORD');
 define('DB_NAME', 'reddit-booru');
 
+// Global AWS S3 enabler
+define('AWS_ENABLED', true);
+
+// If S3 is turned on, information for connecting
 define('AWS_KEY', 'KEY');
 define('AWS_SECRET', 'SECRET');
-define('AWS_ENABLED', true);
 define('AWS_BUCKET', 'BUCKET');
 define('AWS_PATH', 'PATH/');
 
@@ -18,10 +21,17 @@ define('AWS_EXPIRATION', 630720000);
 // View directory
 define('VIEW_PATH', './view/');
 
+// API key for fetching images from tumblr
+define('TUMBLR_CONSUMER_KEY', 'TUMBLR_CONSUMER_KEY');
+
+// Custom user-agent when making outband calls to services. Reddit particularly likes custom UAs
 define('HTTP_UA', 'moe downloader by /u/dxprog');
 
+// Port that the SauceNao service runs on
 define('SAUCENAO_PORT', 'SAUCENAO_SERVICE_PORT');
 
+// Client-ID for imgur API calls
 define('IMGUR_CLIENT_ID', 'REGISTERED_APP_ID');
 
+// Reddit username of the bot (if enabled)
 define('RB_BOT', 'ai-tan');
