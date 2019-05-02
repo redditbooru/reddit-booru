1ce50a70df9ed5dddb03e03539f318018e72211f
Matt Hackmann <matt@dxprog.com>
2019-05-01 23:05:42 -0700

    build settings for dev/prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './static/js/App.js',
   devtool: 'inline-source-map',
   module: {
